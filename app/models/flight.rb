609b0ab743cd6740ac2d478e7382734c150f551c
jasper <jasper@bootyard.com>
2023-02-07 11:17:52 +0800

    Styling forms

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,8 +4,8 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
-  validates :departure_time, presence: true
-  validates :duration, presence: true
+  # validates :departure_time, presence: true
+  # validates :duration, presence: true
 
 
   def self.users_search(query_params)
